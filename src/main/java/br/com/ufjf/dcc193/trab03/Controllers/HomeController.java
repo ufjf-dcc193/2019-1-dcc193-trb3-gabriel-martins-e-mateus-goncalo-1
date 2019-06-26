a03953c5a0803740d7b6ef1a0fd2c180080c7dcb
Mateus Gonçalo <mateusgon57@gmail.com>
2019-06-26 08:06:33 -0300

    Adicionando index.html e o style css

---
+++ src/main/java/br/com/ufjf/dcc193/trab03/Controllers/HomeController.java
@@ -0,0 +1,33 @@
+package br.com.ufjf.dcc193.trab03.Controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping({"", "/", "/index", "/principal-adm", "/principal-usuario"})
+    public String index (HttpSession session)
+    {
+        if (session.getAttribute("usuarioLogado") != null)
+        {   
+//            Usuario usuario = (Usuario) session.getAttribute("usuarioLogado");
+//            if (usuario.getEmail().equals("admin"))
+//            {
+//                return "principal-adm";
+//            }
+//            else
+//            {
+                return "principal-usuario";
+ //           }
+        }
+        else
+        {
+            return "index";
+        }
+    }
+
+
+}
