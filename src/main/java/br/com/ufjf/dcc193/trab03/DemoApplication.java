e180453ac805ed08789b9adb002647d805f683d4
Mateus Gonçalo <mateusgon57@gmail.com>
2019-06-26 07:42:17 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/ufjf/dcc193/trab03/DemoApplication.java
@@ -0,0 +1,14 @@
+package br.com.ufjf.dcc193.trab03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("SIU");
+	}
+
+}
