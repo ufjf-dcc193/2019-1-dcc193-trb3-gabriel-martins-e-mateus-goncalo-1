a7ba69c6ebfedd639789f842cd515dbbeeaaa444
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2019-06-27 14:42:18 -0300

    Adicionando model de etiqueta e o repository

---
+++ src/main/java/br/com/ufjf/dcc193/trab03/Models/Etiqueta.java
@@ -0,0 +1,67 @@
+package br.com.ufjf.dcc193.trab03.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Etiqueta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private String url;
+
+    public Etiqueta(Long id, String titulo, String descricao, String url) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.url = url;
+    }
+
+    public Etiqueta(String titulo, String descricao, String url) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.url = url;
+    }
+
+    public Etiqueta() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    
+}
